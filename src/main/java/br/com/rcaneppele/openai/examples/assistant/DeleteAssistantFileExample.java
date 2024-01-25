9648672ede508baa536aaf85684ceef6fdaf28eb
Rodrigo Caneppele <r.caneppele@gmail.com>
2024-01-25 14:01:18 -0300

    Delete Assistant File example

---
+++ src/main/java/br/com/rcaneppele/openai/examples/assistant/DeleteAssistantFileExample.java
@@ -0,0 +1,23 @@
+package br.com.rcaneppele.openai.examples.assistant;
+
+import br.com.rcaneppele.openai.OpenAIClient;
+import br.com.rcaneppele.openai.error.exception.NotFoundException;
+
+public class DeleteAssistantFileExample {
+
+    public static void main(String[] args) {
+        var apiKey = System.getenv("OPENAI_API_KEY");
+        var client = new OpenAIClient(apiKey, 30);
+
+        try {
+            var deletionStatus = client.deleteAssistantFile("asst_id", "file-id");
+
+            System.out.println("Id: " +deletionStatus.id());
+            System.out.println("Object: " +deletionStatus.object());
+            System.out.println("Deleted: " +deletionStatus.deleted());
+        } catch (NotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
