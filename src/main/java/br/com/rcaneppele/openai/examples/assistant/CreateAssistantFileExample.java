f7bc10bca3f179236fde9d5df2621ff6ebc9aad0
Rodrigo Caneppele <r.caneppele@gmail.com>
2024-01-25 13:48:54 -0300

    Create Assistant File example

---
+++ src/main/java/br/com/rcaneppele/openai/examples/assistant/CreateAssistantFileExample.java
@@ -0,0 +1,24 @@
+package br.com.rcaneppele.openai.examples.assistant;
+
+import br.com.rcaneppele.openai.OpenAIClient;
+import br.com.rcaneppele.openai.endpoints.assistant.request.builder.CreateAssistantFileRequestBuilder;
+
+public class CreateAssistantFileExample {
+
+    public static void main(String[] args) {
+        var apiKey = System.getenv("OPENAI_API_KEY");
+        var client = new OpenAIClient(apiKey, 30);
+
+        var request = new CreateAssistantFileRequestBuilder()
+                .fileId("file-id")
+                .build();
+
+        var assistantFile = client.createAssistantFile("asst_id", request);
+
+        System.out.println("Response id: " +assistantFile.id());
+        System.out.println("Object: " +assistantFile.object());
+        System.out.println("Created at: " +assistantFile.createdAt());
+        System.out.println("Assistant id: " +assistantFile.assistantId());
+    }
+
+}
