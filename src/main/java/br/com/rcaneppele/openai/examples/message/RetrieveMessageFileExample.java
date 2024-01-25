a607fec6a988100e753dfc314489c32948b4e95c
Rodrigo Caneppele <r.caneppele@gmail.com>
2024-01-25 14:39:40 -0300

    Retrieve Message File example

---
+++ src/main/java/br/com/rcaneppele/openai/examples/message/RetrieveMessageFileExample.java
@@ -0,0 +1,19 @@
+package br.com.rcaneppele.openai.examples.message;
+
+import br.com.rcaneppele.openai.OpenAIClient;
+
+public class RetrieveMessageFileExample {
+
+    public static void main(String[] args) {
+        var apiKey = System.getenv("OPENAI_API_KEY");
+        var client = new OpenAIClient(apiKey, 30);
+
+        var file = client.retrieveMessageFile("thread_id", "message_id", "file-id");
+
+        System.out.println("id: " +file.id());
+        System.out.println("Object: " +file.object());
+        System.out.println("Created at: " +file.createdAt());
+        System.out.println("Message id: " +file.messageId());
+    }
+
+}
