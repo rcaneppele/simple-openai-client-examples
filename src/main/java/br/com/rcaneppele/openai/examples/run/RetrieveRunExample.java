e01c16913781aa17b79f58c53002f169a3aa2012
Rodrigo Caneppele <r.caneppele@gmail.com>
2024-01-25 18:24:23 -0300

    Retrieve Run example

---
+++ src/main/java/br/com/rcaneppele/openai/examples/run/RetrieveRunExample.java
@@ -0,0 +1,33 @@
+package br.com.rcaneppele.openai.examples.run;
+
+import br.com.rcaneppele.openai.OpenAIClient;
+
+public class RetrieveRunExample {
+
+    public static void main(String[] args) {
+        var apiKey = System.getenv("OPENAI_API_KEY");
+        var client = new OpenAIClient(apiKey, 30);
+
+       var run = client.retrieveRun("thread_id", "run_id");
+
+        System.out.println("Id: " + run.id());
+        System.out.println("Object: " + run.object());
+        System.out.println("Created at: " + run.createAt());
+        System.out.println("Assistant id: " + run.assistantId());
+        System.out.println("Thread id: " + run.threadId());
+        System.out.println("Status: " + run.status());
+        System.out.println("Started at: " + run.startedAt());
+        System.out.println("Expires at: " + run.expiresAt());
+        System.out.println("Cancelled at: " + run.cancelledAt());
+        System.out.println("Failed at: " + run.failedAt());
+        System.out.println("Completed at: " + run.completedAt());
+        System.out.println("Last error: " + run.lastError());
+        System.out.println("Model: " + run.model());
+        System.out.println("Instructions: " + run.instructions());
+        System.out.println("Tools: " + run.tools());
+        System.out.println("File ids: " + run.fileIds());
+        System.out.println("Metadata: " + run.metadata());
+        System.out.println("Required action: " + run.requiredAction());
+    }
+
+}
