a966a3598fd5d6bdcdde0a531b1eab401efc1eb7
Rodrigo Caneppele <r.caneppele@gmail.com>
2024-01-25 14:12:31 -0300

    Retrieve Thread example

---
+++ src/main/java/br/com/rcaneppele/openai/examples/thread/RetrieveThreadExample.java
@@ -0,0 +1,24 @@
+package br.com.rcaneppele.openai.examples.thread;
+
+import br.com.rcaneppele.openai.OpenAIClient;
+import br.com.rcaneppele.openai.error.exception.NotFoundException;
+
+public class RetrieveThreadExample {
+
+    public static void main(String[] args) {
+        var apiKey = System.getenv("OPENAI_API_KEY");
+        var client = new OpenAIClient(apiKey, 30);
+
+        try {
+            var thread = client.retrieveThread("thread_id");
+
+            System.out.println("id: " +thread.id());
+            System.out.println("Object: " +thread.object());
+            System.out.println("Created at: " +thread.createdAt());
+            System.out.println("Metadata: " +thread.metadata());
+        } catch (NotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
