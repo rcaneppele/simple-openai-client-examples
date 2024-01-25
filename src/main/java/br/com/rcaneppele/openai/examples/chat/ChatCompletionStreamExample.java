ceb8d93f1464769375e053d3237c5c80d583bdaa
Rodrigo Caneppele <r.caneppele@gmail.com>
2024-01-25 09:37:04 -0300

    Chat Completion Stream example

---
+++ src/main/java/br/com/rcaneppele/openai/examples/chat/ChatCompletionStreamExample.java
@@ -0,0 +1,30 @@
+package br.com.rcaneppele.openai.examples.chat;
+
+import br.com.rcaneppele.openai.OpenAIClient;
+import br.com.rcaneppele.openai.common.OpenAIModel;
+import br.com.rcaneppele.openai.endpoints.chatcompletion.request.ChatCompletionRequestBuilder;
+
+public class ChatCompletionStreamExample {
+
+    public static void main(String[] args) {
+        var apiKey = System.getenv("OPENAI_API_KEY");
+        var client = new OpenAIClient(apiKey, 30);
+
+        var request = new ChatCompletionRequestBuilder()
+                .model(OpenAIModel.GPT_4_1106_PREVIEW)
+                .systemMessage("You are a helpful assistant")
+                .userMessage("Explain me what is GPT in simple terms")
+                .frequencyPenalty(1.3)
+                .maxTokens(256)
+                .temperature(1.2)
+                .build();
+
+        client.streamChatCompletion(request).subscribe(response -> {
+            var message = response.firstChoiceMessageContent();
+            if (message != null) {
+                System.out.print(message);
+            }
+        });
+    }
+
+}
